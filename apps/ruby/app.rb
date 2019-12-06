0e41c451eb3039cfdd57f1a9dddecf9dd1c0838b
Aurel Canciu <aurelcanciu@gmail.com>
2019-12-06 18:41:06 +0200

    Set ruby app host to 0.0.0.0

--- apps/ruby/app.rb
+++ apps/ruby/app.rb
@@ -5,4 +5,4 @@ app = Proc.new do |env|
   ['200', {'Content-Type' => 'text/html'}, ["This is %s" % name]]
 end
 
-Rack::Handler::WEBrick.run app, Port: 3000
+Rack::Handler::WEBrick.run app, Host: '0.0.0.0', Port: 3000
